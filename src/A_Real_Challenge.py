52832fb387d819ad642314b86378dca80b0c1184
Hasan <hkilzey17@gmail.com>
2022-03-23 11:36:13 +0100

    feat: Closes Solve A Real Challenge #29

---
+++ src/A_Real_Challenge.py
@@ -0,0 +1,7 @@
+# A Real Challenge
+# # Solution by Hasan Kalzi 26-03-2021
+# # Link to problem in Kattis  https://open.kattis.com/problems/areal
+from sys import stdin, stdout
+from math import sqrt
+
+stdout.write(str(4 * sqrt(int(stdin.readline()))))
