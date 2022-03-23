2c06b577c8f6c8206ab3c963e177a10264361d0b
Hasan <hkilzey17@gmail.com>
2022-03-23 10:32:09 +0100

    Feat: Closes Solve Aaah! #1

---
+++ src/Aaah.py
@@ -0,0 +1,9 @@
+# Aaah!
+# Solution by Hasan Kalzi 29-03-2021
+# Link to problem in https://open.kattis.com/problems/aaah
+from sys import stdin, stdout
+
+if len(stdin.readline())< len(stdin.readline()):
+    stdout.write("no")
+else:
+    stdout.write("go")
