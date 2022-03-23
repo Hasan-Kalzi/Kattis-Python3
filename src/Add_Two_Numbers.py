2da750119f818419ac635c0725613450ea01cdd3
Hasan <hkilzey17@gmail.com>
2022-03-23 10:58:59 +0100

    feat: Closes Solve Add Two Numbers #9

---
+++ src/Add_Two_Numbers.py
@@ -0,0 +1,6 @@
+# Add Two Numbers
+# Solution by Hasan Kalzi 21-06-2021
+# Link to problem in Kattis: https://open.kattis.com/problems/addtwonumbers
+from sys import stdin, stdout
+
+stdout.write(str(sum(map(int, stdin.readline().split()))))
