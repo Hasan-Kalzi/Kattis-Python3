4bc6cf018dd9650810c2b92505b858e8538ea2fc
Hasan <hkilzey17@gmail.com>
2022-03-31 20:17:37 +0200

    feat: closes Solve Autori #61

---
+++ src/Py3/Autori.py
@@ -0,0 +1,6 @@
+# Autori
+# Solution by Hasan Kalzi 08-10-2020
+# Link to the problem in Kattis: https://open.kattis.com/problems/autori
+
+for elem in input().split("-"):
+    print(elem[0], end="")
