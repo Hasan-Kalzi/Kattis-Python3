dd2013905a0b8c8aff1efc5ff2269a84dcdcc0a9
Hasan <hkilzey17@gmail.com>
2022-04-13 09:05:11 +0200

    Feat: closes #90

---
+++ src/Py2/Betting.py
@@ -0,0 +1,8 @@
+# Betting
+# Solution by Hasan Kalzi 14-03-2022
+# Link to the problem in Kattis: https://open.kattis.com/problems/betting
+from __future__ import division
+from sys import stdin, stdout
+
+percentage = int(stdin.readline())
+stdout.write(str((1/percentage)*100)+'\n'+str((1/(100-percentage))*100))
