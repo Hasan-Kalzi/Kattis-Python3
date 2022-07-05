ecf5125279e715074206c8f7a2401b2793dda499
Hasan <hkilzey17@gmail.com>
2022-07-05 14:59:04 +0200

    Closes #217

---
+++ src/Py3/Dice_Cup.py
@@ -0,0 +1,13 @@
+# Dice Cup
+# Solution by Hasan Kalzi 08-10-2020
+# Link to problem in Kattis: https://open.kattis.com/problems/dicecup
+
+values = [int(x) for x in input().split()]
+if values[0] > values[1]:
+    x = values[1]
+    y = values[0]
+else:
+    x = values[0]
+    y = values[1]
+for i in range(x + 1, y + 2):
+    print(i)
