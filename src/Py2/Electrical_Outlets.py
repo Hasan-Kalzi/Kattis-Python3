28e1b8f7a3bac2944316f909e93d36cd3922f63d
Hasan <hkilzey17@gmail.com>
2022-07-06 11:18:20 +0200

    Feat: Solve Electrical Outlets

---
+++ src/Py2/Electrical_Outlets.py
@@ -0,0 +1,11 @@
+# Electrical outlets
+# Solution by Hasan Kalzi 10-03-2021
+# Link to problem in Kattis: https://open.kattis.com/problems/electricaloutlets
+import sys
+
+# after range is the inserted number of cups: int(sys.stdin.readline())
+for _ in range(int(sys.stdin.readline())):
+    values = [int(_) for _ in sys.stdin.readline().split()]
+    # The first value of the list is not included and we add 2 because the first one can provide
+    # without connection. and for the same reason the first value is not calculated.
+    sys.stdout.write(str(sum(values[1:len(values)]) - len(values) + 2) + "\n")
