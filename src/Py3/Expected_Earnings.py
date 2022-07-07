64a24f5981f1265bc13a9d64582990e029a215b1
Hasan <hkilzey17@gmail.com>
2022-07-07 14:47:28 +0200

    Closes #276

---
+++ src/Py3/Expected_Earnings.py
@@ -0,0 +1,10 @@
+# Expected Earnings
+# Solution by Hasan Kalzi 06-03-2022
+# Link to problem in Kattis: https://open.kattis.com/problems/cutinline
+from sys import stdin, stdout
+
+n, k, p = map(float, stdin.readline().split())
+if k/n <= p:
+    stdout.write('spela inte!')
+else:
+    stdout.write('spela')
