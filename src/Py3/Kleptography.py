67185b7ec958597568c9b78de65459c77bbdad15
Hasan <hkilzey17@gmail.com>
2022-07-13 14:09:44 +0200

    Closes #482

---
+++ src/Py3/Kleptography.py
@@ -0,0 +1,29 @@
+# Kleptography
+# Solution by Hasan Kalzi 21-11-2020
+# Link to problem in Kattis: https://open.kattis.com/problems/kleptography
+import string
+
+alphabet = list(string.ascii_lowercase)
+# One line with two integers n and m (1≤n≤30, n+1≤m≤100),
+# where n is the length of the keyword as well as the number of letters Mary saw, and m is the length of the text.
+n, m = [int(_) for _ in input().split()]
+# One line with n lower-case letters, the last n letters of the plaintext.
+last = input()
+# One line with m lower-case letters, the whole ciphertext.
+cipher = input()
+key = [1] * n
+answer = last
+for i in range(n):
+    key[i] = ((alphabet.index(cipher[m - i - 1]) - alphabet.index(last[n - 1 - i])) % 26)
+    answer = alphabet[key[i]] + answer
+i = 0
+m = m - n
+j = 0
+for _ in range(m , 0, -1):
+    if i == n:
+        i = 0
+        m = m - n
+    key[i] = (alphabet.index(cipher[m - i - 1]) - key[i]) % 26
+    answer = alphabet[key[i]] + answer
+    i += 1
+print(answer[n:len(answer)])
