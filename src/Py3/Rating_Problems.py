97fa82e3931a889b953c973923b1dca0c11ea464
Hasan <hkilzey17@gmail.com>
2022-07-22 14:33:41 +0200

    Closes #694

---
+++ src/Py3/Rating_Problems.py
@@ -0,0 +1,11 @@
+# Rating Problems
+# Solution by Hasan Kalzi 21-06-2021
+# # Link to problem in Kattis: https://open.kattis.com/problems/ratingproblems
+from sys import stdin, stdout
+
+
+n, k = map(int, stdin.readline().split())
+total = 0
+for i in range(k):
+    total += int(stdin.readline())
+stdout.write(str((total - (3 * (n - k)))/n) + " " + str((total + (3 * (n - k)))/n))
