81fc0f7d4715915ccd7f8738975ef9316625bec8
Hasan <hkilzey17@gmail.com>
2022-07-26 15:24:53 +0200

    Closes #798

---
+++ src/Py2/Solving_for_Carrots.py
@@ -0,0 +1,5 @@
+# Solving for Carrots
+# Solution by Hasan Kalzi 27-09-2020
+# Link to problem in Kattis: https://open.kattis.com/problems/carrots
+from sys import stdin, stdout
+stdout.write(stdin.readline().split()[1])
---
+++ src/Py3/Solving_for_Carrots.py
@@ -0,0 +1,5 @@
+# Solving for Carrots
+# Solution by Hasan Kalzi 27-09-2020
+# Link to problem in Kattis: https://open.kattis.com/problems/carrots
+
+print(input().split()[1])
