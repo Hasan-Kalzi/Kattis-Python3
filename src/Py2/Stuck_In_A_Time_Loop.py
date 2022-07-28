7bd463f5980100e6ab0e8e108dcd66b4596d685d
Hasan <hkilzey17@gmail.com>
2022-07-28 10:08:42 +0200

    Closes #846

---
+++ src/Py2/Stuck_In_A_Time_Loop.py
@@ -0,0 +1,7 @@
+# Stuck In A Time Loop
+# Solution by Hasan Kalzi 16-10-2020
+# Link to problem in Kattis: https://open.kattis.com/problems/timeloop
+from sys import stdin, stdout
+
+for i in range(int(stdin.readline())):
+    stdout.write(str(i + 1) + " Abracadabra\n")
---
+++ src/Py3/Stuck_In_A_Time_Loop.py
@@ -0,0 +1,7 @@
+# Stuck In A Time Loop
+# Solution by Hasan Kalzi 16-10-2020
+# Link to problem in Kattis: https://open.kattis.com/problems/timeloop
+
+amount_words = int(input())
+for i in range(amount_words):
+    print(i+1, "Abracadabra")
