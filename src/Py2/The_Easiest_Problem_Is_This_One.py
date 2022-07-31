ee34f4c5f83cbb990c08cfac1423438e8310c2e5
Hasan <hkilzey17@gmail.com>
2022-07-31 10:09:47 +0200

    Closes #890

---
+++ src/Py2/The_Easiest_Problem_Is_This_One.py
@@ -0,0 +1,17 @@
+# The Easiest Problem Is This One
+# Solution by Hasan Kalzi 11-03-2021
+# Link to problem in https://open.kattis.com/problems/easiest
+import sys
+
+while 1:
+    number = int(sys.stdin.readline())
+    if number == 0:
+        break
+    sum_of_digits = sum(int(_) for _ in str(number))
+    i = 11
+    while True:
+        if sum(int(_) for _ in str(number * i)) == sum_of_digits:
+            sys.stdout.write(str(i) + '\n')
+            break
+        i += 1
+
