10b9f682ad066e7fe7ae76be4112ba0f2391dbb0
Hasan <hkilzey17@gmail.com>
2022-07-31 10:47:56 +0200

    Closes #894

---
+++ src/Py3/The_Last_Problem.py
@@ -0,0 +1,6 @@
+# The Last Problem
+# Solution by Hasan Kalzi 03-04-2021
+# Link to problem in https://open.kattis.com/problems/thelastproblem
+from sys import stdout, stdin
+
+stdout.write("Thank you, {}, and farewell!".format(stdin.readline().strip()))
