9c70b02b41a24251d9d1d6df207092728d12dc1a
Hasan <hkilzey17@gmail.com>
2022-08-01 14:24:01 +0200

    Closes #922

---
+++ src/Py3/Two-sum.py
@@ -0,0 +1,6 @@
+# Two-sum
+# Solution by Hasan Kalzi 31-10-2021
+# Link to problem in Kattis: https://open.kattis.com/problems/twosum
+from sys import stdin, stdout
+
+stdout.write(str(sum(map(int, stdin.readline().split()))))
