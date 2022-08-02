f7f8c95b48760e11c88d9abfcd71d263b936e716
Hasan <hkilzey17@gmail.com>
2022-08-02 08:07:49 +0200

    Closes #950

---
+++ src/Py2/Yin_Yang_Stones.py
@@ -0,0 +1,20 @@
+# Yin and Yang Stones
+# Solution by Hasan Kalzi 07-04-2021
+# Link to problem in https://open.kattis.com/problems/yinyangstones
+from __future__ import division
+from sys import stdin, stdout
+
+w, b, stones = 0, 0, stdin.readline().strip()
+if stones == "WB" * int(len(stones) / 2) or stones == "BW" * int(len(stones) / 2):
+    stdout.write('0')
+else:
+    for char in stones:
+        if char == 'W':
+            w += 1
+        elif char == 'B':
+            b += 1
+
+    if w == b and w > 2:
+        stdout.write('1')
+    else:
+        stdout.write('0')
