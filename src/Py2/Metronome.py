1efec7c2c55e4b55982a28b534c13227d034f2f6
Hasan <hkilzey17@gmail.com>
2023-04-05 13:58:33 +0200

    Closes #1018

---
+++ src/Py2/Metronome.py
@@ -0,0 +1,8 @@
+# Metronome
+# Solution by Hasan Kalzi 05-04-2023
+# Link to problem in Kattis: https://open.kattis.com/problems/metronome
+from __future__ import division
+from sys import stdout, stdin
+
+
+stdout.write(str((int(stdin.readline()) / 4)))
