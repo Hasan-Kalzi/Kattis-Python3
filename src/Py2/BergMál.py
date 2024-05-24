4f6e9002825c1daeeea3913ac0a160b9858018d5
Hasan Kalzi <hkilzey17@gmail.com>
2024-05-24 14:35:26 +0200

    closes #1070

---
+++ src/Py2/BergMál.py
@@ -0,0 +1,7 @@
+# BergMál
+# Solution by Hasan Kalzi 24-05-2024
+# Link to the problem in Kattis: https://open.kattis.com/problems/bergmal
+from sys import stdout, stdin
+
+
+stdout.write(stdin.readline())
